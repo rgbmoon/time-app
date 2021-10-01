f7fc47ffc0465d37a8ca0c11b37a1485d90bcef0
Sergei <sergeiuniverse@gmial.com>
2021-10-01 14:54:44 +0300

    Классовый вариант

--- src/App.js
+++ src/App.js
@@ -1,51 +1,63 @@
+import React from 'react';
 import './App.css'
-import React, { useState, useEffect } from "react"
 import { format } from 'date-fns'
 import { ru } from 'date-fns/locale'
 import 'animate.css'
 
 
-/* Функциональный вариант. 
-Я честно не понял как работает useState и useEffect
-Использовал эти функции по примеру из интернетов
-НО!!! Оно живое и работает!) */
+/* Классовый вариант 
+В плане понимания более интуитивный чем функц. и хуки
+Методы состояния позаимствовал в спецификации реакта
+Пока что не могу сказать, что на 100% разобрался, как
+оно работатет, но блин работает же!)*/
 
-function Time() {
+class Time extends React.Component {
 
-  const [currentDate, setDate] = useState(new Date())
-
-  function refreshClock() {
-    setDate(new Date());
+  constructor(props) {
+    super(props)
+    this.state = { currentDate: new Date() }
   }
 
-  useEffect(() => {
-    const timerId = setInterval(refreshClock, 100);
-    return function cleanup() {
-      clearInterval(timerId);
-    };
-  }, []);
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      100
+    );
+  }
 
-  return (
-    <div className="App">
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      currentDate: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
         <div className="timeWrapper">
-          <div className="timeItem">
-            {format(currentDate, 'dd.MM.yyyy')}
+          <div className="timeItem timeItem animate__animated animate__fadeIn animate__slower">
+            {format(this.state.currentDate, 'dd.MM.yyyy')}
           </div>
-          <div className="timeItem">
-            {format(currentDate, 'do MMMM', {locale: ru})}
+          <div className="timeItem timeItem animate__animated animate__fadeIn animate__slower">
+            {format(this.state.currentDate, 'do MMMM', { locale: ru })}
           </div>
-          <div className="timeItem">
-            {format(currentDate, 'HH.mm')}
+          <div className="timeItem timeItem animate__animated animate__fadeIn animate__slower">
+            {format(this.state.currentDate, 'HH.mm')}
           </div>
-          <div className="timeItem">
-            {format(currentDate, 's')}
+          <div className="timeItem timeItem animate__animated animate__fadeIn animate__slower">
+            {format(this.state.currentDate, 's')}
           </div>
-          <div className="timeItem">
-            {currentDate.getMilliseconds()}
+          <div className="timeItem timeItem animate__animated animate__fadeIn animate__slower">
+            {this.state.currentDate.getMilliseconds()}
           </div>
         </div>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default Time
